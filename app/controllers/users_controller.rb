6b39d38b468fd96100388c83b1779de9a06380b1
victoria <vbodnar99@gmail.com>
2017-10-20 10:23:41 +0300

    about number

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,56 @@
 class UsersController < ApplicationController
-	before_action :user_session, exept: [:new, :create, :login, :loginresult]
+  before_action :user_session, except: [:new, :create, :login, :loginresult]
 
-def new
-end
 
-def create
-	@user = User.new(user_params)
-	@user.save
-	redirect_to root_path
-end
+  def new
+    @user=User.new
+  end
 
-def login
-end
+  def create
+    @user =User.new(user_params)
+   if @user.save
+    redirect_to root_path
+   else
+    render 'new'
+   end
+    end
 
-def loginresult
-    pp params[:users][:email]
-    pp params[:users][:password]
-    pp @user = User.find_by_email_and_password(params[:users][:email], params[:users][:password])
-    if @user!= nil
-    session[:id] = @user.id
+  def login
+  end
+
+  def loginresult
+      @user =User.find_by_email_and_password(params[:user][:email],params[:user][:password])
+    if @user!=nil
+      session[:id]=@user.id
+      redirect_to user_path(@user.id)
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
     redirect_to user_path(@user.id)
+  end
 
-end
-end
-def show
+  def destroy
+    session[:id] = nil
+    redirect_to root_path
+  end
 
-	end
+private
 
-	def edit
-		
-	end
-
-	def update
-		@user.update(user_params)
-		redirect_to user_path(@user.id)
-	end
-
-	def destroy
-session[:id] = nil
-redirect_to root_path
-	end
-
-private 
-
-def user_params
-	params.require(:user).permit(:lastname, :firstname, :password, :email)
-end
-
-def user_session
-	@user = User.find(session[:id])
-	end
+  def user_params
+    params.require(:user).permit(:firstname,:lastname,:age,:phone,:email,:password,:country,:city,:street,:apt,:link,:gender)
+  end 
+  
+  def user_session
+    @user =User.find(session[:id])
+  end
 
 
 end
